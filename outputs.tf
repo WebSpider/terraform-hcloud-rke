7611837627080f83cdcaa391f427eb97e86973c9
Nils Vogels <n.vogels@aves-it.nl>
2020-04-16 13:52:17 +0200

    Add useful outputs for kubernetes provider authentication

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,28 @@ output "hcloud_network_id" {
   value = hcloud_network.this.id
 }
 
+output "kube_config_path" {
+  description = "Contents of the kube_config file"
+  value = local_file.kube_cluster_yaml.filename
+}
+
+output "api_server_url" {
+  description = "URL of the Kubernetes API server"
+  value = rke_cluster.this.api_server_url
+}
+
+output "client_cert" {
+  description = "Admin client certificate"
+  value = rke_cluster.this.client_cert
+}
+
+output "client_key" {
+  description = "Admin client key"
+  value = rke_cluster.this.client_key
+}
+
+output "ca_cert" {
+  description = "Cluster CA Certificate"
+  value = rke_cluster.this.ca_crt
+}
+
